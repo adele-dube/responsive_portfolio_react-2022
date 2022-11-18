9260ea5c444947a76b7db4c6d44c081e93518b0b
Adele_D <elmaadeledube@gmail.com>
2022-11-18 13:24:58 +0200

    Created Content Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Hero from "./Hero";
 import "bootstrap/dist/css/bootstrap.css";
+import Content from "./Content";
 
 export default function App() {
   return (
     <div className="App">
       <Hero />
+      <Content />
     </div>
   );
 }
---
+++ src/Content.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "./Content.css";
+
+import ReactDOM from "react-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+
+export default function Content() {
+  return (
+    <div className="Content">
+      <div className="container">banana</div>
+    </div>
+  );
+}
